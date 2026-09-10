fdd7eeacae6780e62642ad5ab9940229f472eb6b
geojoshuaaquino-source <geo.joshua.aquino@gmail.com>
2026-09-10 23:53:14 +0800

    Remove obsolete ButtonLine Dp compatibility shim

--- app/src/main/java/com/draftlock/app/ButtonLineDpCompat.kt
+++
@@ -1,9 +0,0 @@
-package com.draftlock.app
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.Dp
-
-@Composable
-private fun ButtonLine(text: String, y: Dp, onClick: () -> Unit) {
-    ButtonLine(text, y.value.toInt(), onClick)
-}
