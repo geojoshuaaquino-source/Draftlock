c6519a5ec5ad1389035b1cb598321659e365a941
geojoshuaaquino-source <geo.joshua.aquino@gmail.com>
2026-09-10 19:33:37 +0800

    Fix AppAuth redirect placeholder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         versionCode = 1
         versionName = "0.1.0"
         buildConfigField("String", "GOOGLE_CLIENT_ID", "\"$googleClientId\"")
-        manifestPlaceholders["appAuthRedirectScheme"] = "com.google.android.gms.auth.api.credentials"
+        manifestPlaceholders["appAuthRedirectScheme"] = "com.googleusercontent.apps.$redirectPrefix"
     }
 
     buildFeatures {
